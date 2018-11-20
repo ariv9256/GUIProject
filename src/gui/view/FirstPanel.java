f8b4aed022d3dd399a8f48ca435a919a1277b348
Angela Rivas <snowcats001@gmail.com>
2018-11-20 12:37:26 -0700

    Added 3 private methods for the panel, created reference in constructor

--- src/gui/view/FirstPanel.java
+++ src/gui/view/FirstPanel.java
@@ -2,7 +2,29 @@ package gui.view;
 
 import javax.swing.JPanel;
 
-public class FirstPanel
+public class FirstPanel extends JPanel
 {
-
+	
+	public FirstPanel()
+	{
+		super();
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+		
+	}
+	private void setupPanel()
+	{
+		
+		
+	}
+	private void setupLayout()
+	{
+		
+	}
+	private void setupListeners()
+	{
+		
+	}
 }
